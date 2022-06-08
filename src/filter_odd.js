bbfcb7e00990b2ffda0a3a1c23ef2fe078ca9df5
viktor-silakov <1105714@gmail.com>
2022-06-08 12:24:41 +0400

    test

---
+++ src/filter_odd.js
@@ -0,0 +1,11 @@
+/**
+ * Write the function that takes array of numbers
+ * and return array only odd numbers from the argument array
+ */
+
+function returnOdd(arr) {
+  return filtered_array;
+}
+
+returnOdd([1, 2, 3]); // expected [1,3]
+returnOdd([1, 2, 3, 4, 5]); // expected [1,3,5]
---
+++ src/sum_arg.js
@@ -0,0 +1,10 @@
+/**
+ Write the function that takes any number of arguments
+ of type number and return the sum of all the arguments
+ */
+function sum() {
+  return 123;
+}
+console.log(sum(1, 2, 3)); // expected 6
+console.log(sum(1, 2, 3, 4)); // expected 10
+//123
